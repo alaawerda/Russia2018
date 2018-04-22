1c0192869a469dbc9a4940e94851281bc3ca19cf
monta <montassar.touil@esprit.tn>
2018-04-22 20:12:33 +0100

    asset

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,7 +6,6 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Twig_Extensions_' => array($vendorDir . '/twig/extensions/lib'),
     'Twig_' => array($vendorDir . '/twig/twig/lib'),
     'SensioLabs\\Security' => array($vendorDir . '/sensiolabs/security-checker'),
     'Doctrine\\ORM\\' => array($vendorDir . '/doctrine/orm/lib'),
